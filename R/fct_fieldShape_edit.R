88bdb8721d32fd116e0de0493ead32b6cd0d006e
Filipe Matias <11415688+filipematias23@users.noreply.github.com>
2023-11-24 14:26:41 -0300

    Update fct_fieldShape_edit.R

--- R/fct_fieldShape_edit.R
+++ R/fct_fieldShape_edit.R
@@ -60,7 +60,7 @@ fieldShape_edit <- function(mosaic,
   } else  {
     stars_object <- mosaic
     if (!inherits(stars_object, "stars")) {
-      stars_object <- st_as_stars(mosaic)
+      stars_object <- st_as_stars(mosaic, proxy = TRUE)
       names(stars_object)<-"layer_name"
      # stars_object <- read_stars(stars_object$layer_name, proxy = TRUE)
       stars_object <- st_downsample(stars_object, n = downsample)
