e0db156281562114ed30fa803198d4553691ca03
Filipe Matias <11415688+filipematias23@users.noreply.github.com>
2023-11-24 14:27:30 -0300

    Update fct_fieldView.R

--- R/fct_fieldView.R
+++ R/fct_fieldView.R
@@ -72,7 +72,7 @@ fieldView <- function(mosaic = NULL,
   } else {
     stars_object <- mosaic
     if (!inherits(stars_object, "stars")) {
-      stars_object <- st_as_stars(mosaic)
+      stars_object <- st_as_stars(mosaic, proxy = TRUE)
       names(stars_object)<-"layer_name"
       # stars_object <- read_stars(stars_object$layer_name, proxy = TRUE)
       stars_object <- st_downsample(stars_object, n = downsample)
