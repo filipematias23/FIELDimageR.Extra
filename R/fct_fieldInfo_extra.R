809bb58a916c201114e78aa8d0a81efa788c4f63
pspawar71 <83626626+pspawar71@users.noreply.github.com>
2024-02-01 10:48:49 +0530

    Update fct_fieldInfo_extra.R

--- R/fct_fieldInfo_extra.R
+++ R/fct_fieldInfo_extra.R
@@ -11,32 +11,42 @@
 #' @param progress if TRUE, display a progress bar during processing
 #'
 #' @importFrom sf st_crs st_transform st_as_sf st_join
-#' @importFrom terra rast extract
+#' @importFrom exactextractr exact_extract
 #' @importFrom stars write_stars st_warp st_as_stars st_extract
 #'
 #' @return A data frame class "sf" with values by plot.
 #'
 #' @export
 fieldInfo_extra <- function(mosaic,
-                            fieldShape,
-                            fun = "mean",
-                            progress = FALSE) {
+                             fieldShape,
+                             fun = "mean",
+                             progress = FALSE) {
   print("Starting data extraction per plot ...")
+  
   if (is.null(mosaic)) {
     stop("The input 'mosaic' object is NULL.")
   }
+  
   if (class(mosaic) %in% c("RasterStack", "RasterLayer", "RasterBrick")) {
     mosaic <- terra::rast(mosaic)
   }
-  plotInfo <- exactextractr::exact_extract(
-    x = mosaic,
-    y = fieldShape,
-    fun = fun,
-    progress = progress,
-    force_df = TRUE
-  )
-  colnames(plotInfo) <- gsub(".*\\.", "", colnames(plotInfo))
-  Out <- cbind(fieldShape, plotInfo)
-  print("End!")
-  return(Out)
+  
+  valid_functions <- c("mean", "sum", "min", "max", "summary")
+  if (!(fun %in% valid_functions)) {
+    stop("Use one of the functions from ('mean', 'sum', 'min', 'max', 'summary').")
+  }
+  
+  if (fun == "summary") {
+    fun <- c('mean', 'sum', 'max', 'min', 'mode', 'stdev', 'variance', 'coefficient_of_variation', 'majority', 'minority')
+  }
+  
+  plotInfo <- exactextractr::exact_extract(x = mosaic, y = fieldShape, fun = fun, progress = progress, force_df = TRUE)
+  
+  if (length(fun) == 1) {
+    colnames(plotInfo) <- paste0(names(mosaic), '_', fun)
+  } else {
+    colnames(plotInfo) <- paste0(names(mosaic), '_', fun)
+  }
+  
+  return(plotInfo)
 }
